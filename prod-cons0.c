578fb5059abf2181d035509189c7cc02efd47895
mikalaki <mikalakis@hotmail.gr>
2020-03-20 00:49:46 +0200

    rough implementation prod-cons with thread function queues

---
+++ prod-cons0.c
@@ -0,0 +1,192 @@
+/*
+ *	File	: pc.c
+ *
+ *	Title	: Demo Producer/Consumer.
+ *
+ *	Short	: A solution to the producer consumer problem using
+ *		pthreads.
+ *
+ *	Long 	:
+ *
+ *	Author	: Andrae Muys
+ *
+ *	Date	: 18 September 1997
+ *
+ *	Revised	:
+ */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+//testing
+#define QUEUESIZE 5
+#define LOOP 5
+
+struct workFunction {
+  void * (*work)(void *);
+  void * arg;
+};
+
+//Threads functions declaration.
+void *producer (void *args);
+void *consumer (void *args);
+
+//declaration of a workFuction for the queue
+void * func1(void * arg);
+
+//Struct defined for the queue implementation
+typedef struct {
+  struct workFunction buf[QUEUESIZE];
+  long head, tail;
+  int full, empty;
+  pthread_mutex_t *mut;
+  pthread_cond_t *notFull, *notEmpty;
+} queue;
+
+//queue methods (functions) definition
+queue *queueInit (void);
+void queueDelete (queue *q);
+
+void queueAdd (queue *q, struct workFunction in);
+void queueExec (queue *q);
+
+int main ()
+{
+  queue *fifo; //queue declaration
+  pthread_t pro, con; //threads declaration
+
+  fifo = queueInit (); //queue initialization
+  if (fifo ==  NULL) {
+    fprintf (stderr, "main: Queue Init failed.\n");
+    exit (1);
+  }
+  pthread_create (&pro, NULL, producer, fifo);
+  pthread_create (&con, NULL, consumer, fifo);
+  pthread_join (pro, NULL);
+  pthread_join (con, NULL);
+  queueDelete (fifo);
+
+  return 0;
+}
+
+void *producer (void *q)
+{
+  queue *fifo; //pointer to a queue struct item
+  int i;
+
+  fifo = (queue *)q; //casting
+
+  for (i = 0; i < LOOP; i++) {
+    pthread_mutex_lock (fifo->mut); //locking
+    while (fifo->full) {
+      printf ("producer: queue FULL.\n");
+      pthread_cond_wait (fifo->notFull, fifo->mut); // when recieves the signal, it waits to lock the mut and continue
+    }
+    //void queueAdd (queue *q, struct workFunction in)
+
+    // Creating the struct about the workFuction to add in the queue
+    struct workFunction  func;
+    // func=(struct workFunction *) malloc(sizeof(struct workFunction ));
+    func.work = func1;
+    func.arg = (void *)i;
+
+    //the function that adds a test function in the queue
+    queueAdd (fifo, func);
+
+    pthread_mutex_unlock (fifo->mut); //unlocking
+    pthread_cond_signal (fifo->notEmpty); //signal in case cons thread is blocked
+  }
+
+  return (NULL);
+}
+
+void *consumer (void *q)
+{
+  queue *fifo;
+  int i;
+  //struct workFunction  d;
+
+  fifo = (queue *)q;
+
+  for (i = 0; i < LOOP; i++) {
+    pthread_mutex_lock (fifo->mut);
+    while (fifo->empty) {
+      printf ("consumer: queue EMPTY.\n");
+      pthread_cond_wait (fifo->notEmpty, fifo->mut);
+    }
+    queueExec (fifo);
+    pthread_mutex_unlock (fifo->mut);
+    pthread_cond_signal (fifo->notFull);
+    //printf ("consumer: recieved %d.\n", d);
+    // usleep(200000);
+  }
+  return (NULL);
+}
+
+queue *queueInit (void)
+{
+  queue *q;
+
+  q = (queue *)malloc (sizeof (queue));
+  if (q == NULL) return (NULL);
+
+  q->empty = 1;
+  q->full = 0;
+  q->head = 0;
+  q->tail = 0;
+  q->mut = (pthread_mutex_t *) malloc (sizeof (pthread_mutex_t));
+  pthread_mutex_init (q->mut, NULL);
+  q->notFull = (pthread_cond_t *) malloc (sizeof (pthread_cond_t));
+  pthread_cond_init (q->notFull, NULL);
+  q->notEmpty = (pthread_cond_t *) malloc (sizeof (pthread_cond_t));
+  pthread_cond_init (q->notEmpty, NULL);
+
+  return (q);
+}
+
+void queueDelete (queue *q)
+{
+  pthread_mutex_destroy (q->mut);
+  free (q->mut);
+  pthread_cond_destroy (q->notFull);
+  free (q->notFull);
+  pthread_cond_destroy (q->notEmpty);
+  free (q->notEmpty);
+  free (q);
+}
+
+void queueAdd (queue *q, struct workFunction in)
+{
+  q->buf[q->tail] = in;
+  q->tail++;
+  if (q->tail == QUEUESIZE)
+    q->tail = 0;
+  if (q->tail == q->head)
+    q->full = 1;
+  q->empty = 0;
+
+  return;
+}
+
+void queueExec (queue *q)
+{
+  //executing a work function
+  struct workFunction out = q->buf[q->head];
+  (out.work)(out.arg);
+
+  q->head++;
+  if (q->head == QUEUESIZE)
+    q->head = 0;
+  if (q->head == q->tail)
+    q->empty = 1;
+  q->full = 0;
+  return;
+
+}
+
+//a Work function implementation ( definition)
+void * func1(void * arg){
+  int i = (int) arg;
+  printf("I am function with i=%d \n", i);
+}
