92946860416e505beafec19c12df7159cdf3fb2a
mikalaki <mikalakis@hotmail.gr>
2020-03-28 02:48:46 +0200

    Added , -O3 flag, in bench script

--- bench.sh
+++ bench.sh
@@ -2,7 +2,7 @@
 #This bash script is used for compiling and benching the prod-cons_multithreading.c programm
 
 #compiling
-gcc prod-cons_multithreading.c -pthread -o program.out -lm
+gcc prod-cons_multithreading.c -pthread -o program.out -lm -O3
 
 #benching the program
 for P in 1 2 4 6 8 10 12 16
